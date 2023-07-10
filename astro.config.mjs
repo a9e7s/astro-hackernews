99f29568c0cdb25ef368f97eacd9497ebaba44f5
刘珂珂 <ifme.in@gmail.com>
2023-07-10 16:32:50 +0800

    custom index

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,13 @@ import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
-	output: 'server',
-	adapter: node({
-		mode: 'standalone',
-	}),
+  output: 'server',
+  adapter: node({
+    mode: 'standalone',
+  }),
+  resolve: {
+    alias: {
+      '~': new URL('./src', import.meta.url),
+    },
+  },
 });
